9d115f52a04e2d5caa4d20984aeac201835f012a
49N <soz.sayed.youssef@gmail.com>
2024-02-22 20:09:48 +0100

    added animation

--- scripts/entities.py
+++ scripts/entities.py
@@ -8,10 +8,21 @@ class PhysicsEntity:
         self.size = size
         self.velocity = [0, 0]
         self.collisons = {'up': False, 'down': False, 'right': False, 'left': False}
+
+        self.action = ''
+        self.anim_offset = (-3, -3)
+        self.flip = False
+        self.set_action('idle')
         
     def rect(self): # Rect för spelaren
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
-        
+
+    # Ändrar animation beroende på vad spelaren gör
+    def set_action(self, action):
+        if action != self.action:
+            self.action = action
+            self.animation = self.game.assets[self.type + '/' + self.action].copy()
+
     def update(self, tilemap, movement = (0, 0)):
         self.collisons = {'up': False, 'down': False, 'right': False, 'left': False}
 
@@ -42,12 +53,42 @@ class PhysicsEntity:
                     entity_rect.top = rect.bottom
                     self.collisons['up'] = True
                 self.pos[1] = entity_rect.y
-                
+
+        #karaktären rör sig höger så flippa ej
+        if movement[0] > 0:
+            self.flip = False
+        #karaktären rör sig left så flippa bilden
+        if movement[0] < 0:
+            self.flip = True
+
         # Gravitation
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
         
         if self.collisons['down'] or self.collisons['up']:
             self.velocity[1] = 0
         
+        self.animation.update()
+
     def render(self, surf, offset = (0, 0)):
-        surf.blit(self.game.assets['player'], (self.pos[0] - offset[0], self.pos[1] - offset[1]))
+        surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1]))
+
+class Player(PhysicsEntity):
+    def __init__(self, game, pos, size):
+        super().__init__(game, 'player', pos, size)
+        self.air_time = 0
+
+    def update(self, tilemap, movement=(0,0)):
+        super().update(tilemap, movement=movement)
+
+        self.air_time += 1
+        if self.collisons['down']:
+            self.air_time = 0
+
+        # När karaktären är i luften visas jump animationen
+        # air_time har högre prioritet än movement för att animationen ska inte visa att karaktären springer i luften    
+        if self.air_time > 4:
+            self.set_action('jump')
+        elif movement[0] != 0:
+            self.set_action('run')
+        else:
+            self.set_action('idle')
